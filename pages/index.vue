3c2170756a0ee139371f7961fcf4eae04f7ba717
Vince489 <vincentcornelius489@gmail.com>
2023-02-14 17:54:46 -0500

    hey

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <h1>I'm home!</h1><hr>
-    <form>
-      <input type="text" id="gamerTag" placeholder="Gamertag" required>
-      <input type="email" id="email" placeholder="Email" required>
-      <input v-on="click" type="submit" id="submit" placeholder="submit">
-
+    <form @submit.prevent="login">
+      <label for="gamerTag">login: </label>
+      <input type="text" id="gamerTag" name="gamerTag" v-model="gamerTag" placeholder="Gamertag" required>
+      <input type="email" id="email" v-model="email" placeholder="Email" required>
+      <input type="submit" id="submit" placeholder="submit">
     </form>
+    <p>Gamertag: {{ gamerTag }} </p>
+    <p>Email: {{ email }} </p>
   </div>
 </template>
 
 <script setup>
+
+import { ref } from 'vue'
+
+const gamerTag = ref(null)
+const email = ref(null)
+
+
+const login = () => {
+  console.log(gamerTag.value)
+  console.log(email.value)
+  return (gamerTag.value, email.value)
+}
 
 
 
