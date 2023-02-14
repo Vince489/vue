8cd57ebd513c682afdd30b42db816d69d5827257
Vince489 <vincentcornelius489@gmail.com>
2023-02-13 21:28:12 -0500

    v-on

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>I'm home!</h1><hr>
+    <form>
+      <input type="text" id="gamerTag" placeholder="Gamertag" required>
+      <input type="email" id="email" placeholder="Email" required>
+      <input v-on="click" type="submit" id="submit" placeholder="submit">
+
+    </form>
+  </div>
+</template>
+
+<script setup>
+
+
+
+</script>
+
+<style scoped>
+
+</style>
